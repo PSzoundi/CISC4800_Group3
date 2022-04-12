bff47a9a54a9387ac4cc632b0560cf843779f8ba
Shakhram <shakag6@gmail.com>
2022-04-12 16:46:31 -0400

    Dipict more weather conditions on the page

--- cisc4800_group3/src/components/Weather.js
+++ cisc4800_group3/src/components/Weather.js
@@ -8,6 +8,7 @@ export default function Weather() {
     let navigate = useNavigate()
 
     const [weatherData, setWeatherData] = useState('')
+    const [tempOption, setTempOption] = useState('')
     const MY_KEY = process.env.REACT_APP_WEATHER_API_KEY
 
     useEffect(() => {
@@ -16,6 +17,20 @@ export default function Weather() {
 
     const goToHome = () => {
         navigate('/')
+    }
+
+    const handleFormOptions = (e) => {
+        setTempOption(...tempOption, tempOption => e.target.name)
+
+        console.log(tempOption)
+        console.log(e.target.name)
+    }
+
+    const changeFormOptions = (e) => {
+        setTempOption(...tempOption, tempOption => e.target.name)
+
+        console.log(tempOption)
+        console.log(e.target.name)
     }
 
     const fetchWeatherData = async () => {
@@ -37,13 +52,17 @@ export default function Weather() {
                 <div className="weather-class">
                     <button onClick={goToHome}>HOME</button>
                     <h1>You Entered: {weatherData.location.name}</h1>
-                    <form>
+                    <form onClick={handleFormOptions}>
                         <select id='temp-select'>
-                            <option value='Fahrenheit'>Fahrenheit</option>
-                            <option value='Celsius'>Celsius</option>
+                            <option value='Fahrenheit' name='Fahrenheit' onChange={changeFormOptions}>Fahrenheit</option>
+                            <option value='Celsius' name='Celsius' onChange={changeFormOptions}>Celsius</option>
                         </select>
                     </form>
                     <h1>Current Weather: {weatherData.current.temp_f}</h1>
+                    <h1>Conditions: {weatherData.current.condition.text}</h1>
+                    <img src={weatherData.current.condition.icon}/>
+                    <h1>Humidity: {weatherData.current.humidity}%</h1>
+                    <h1>Wind: {weatherData.current.wind_mph}mph</h1>
                 </div> : <></>
             }
         </>
