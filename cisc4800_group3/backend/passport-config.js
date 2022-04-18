522ad5e45b4b9130d228fc2afd404e568ff7a9d7
Shakhram <shakag6@gmail.com>
2022-04-18 19:10:51 -0400

    Set up backend and display login/signup forms through temp ejs files

---
+++ cisc4800_group3/backend/passport-config.js
@@ -0,0 +1,34 @@
+const { authenticate } = require('passport')
+const bcrypt = require('bcrypt')
+
+const LocalStrategy = require('passport-local').Strategy
+
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email)
+        if (user == null) {
+            return done(null, false, { message: 'No user with that email' })
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: 'Password incorrect' })
+            }
+        } catch (e) {
+            return done(e)
+        }
+    }
+
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+
+    //determines which data of the user object should be stored in the session
+    //when you go betweenn different places, you want to make sure the user is verified within all those pages
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        done(null, getUserById(id))
+    })
+}
+
+module.exports = initialize
